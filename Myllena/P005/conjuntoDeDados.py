c3f7696e68ebe02c48fc5e55062d24123109124c
Myllena <mssilva.egq@uesc.br>
2024-01-13 18:08:04 -0300

    criando listas

--- Myllena/P005/conjuntoDeDados.py
+++ Myllena/P005/conjuntoDeDados.py
@@ -1,11 +1,13 @@
 import pandas as pd
-import random 
+import random
+ 
+#colocando as informações em ordem alfabetica (alana, gabriel, girleide, leane e myllena)
 
-identificador =[]
-idade = []
-formacao = []
-formacaoGeral = []
-formacaoEspecifica =[]
-andamentoGraduacao = []
-tempoFormacao = []
-experiencciaPrevia = []
+identificador =['tic18Py06799', 'tic18Py07100', 'tic18Py08099', 'tic18Py07799' 'tic18Py05996']
+idade = [24, 23, 24, 24, 27]
+formacao = [3, 2, 2, 2, 1]
+formacaoGeral = [1, 0, 0, 0]
+formacaoEspecifica =['Ciência da Computação', 'Engenharia Elétrica', 'Engenharia Química', 'Engenharia Química', 'Engenharia Química']
+andamentoGraduacao = [None, 90.16, 75.58, 76.94, 81.38]
+tempoFormacao = [1, None, None, None, None]
+experiencciaPrevia = [True, True, True, True, True]
